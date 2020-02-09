0f5905b9746cdb1a922c54e75a69f65eef5245f6
joeyHarig <55154968+joeyHarig@users.noreply.github.com>
2020-02-09 11:18:35 -0700

    GA Event tracking

--- src/components/uiElements/SocialLinks.js
+++ src/components/uiElements/SocialLinks.js
@@ -1,32 +1,22 @@
 import React from 'react';
-
-import Icon from '../icons/Icon';
+import SocialLink from './SocialLink';
 import './social-links.scss';
 
 const SocialLinks = () => {
     return (
         <div className="social-links">
-            <a 
-                target="_blank"
-                href="https://www.linkedin.com/in/joey-harig-256a05167/"
-                rel="noopener noreferrer"
-            >
-                <Icon icon="linkedin" />
-            </a>
-            <a 
-                target="_blank" 
-                href="https://github.com/joeyHarig"
-                rel="noopener noreferrer"
-            >
-                <Icon icon="github" />
-            </a>
-            <a 
-                target="_blank" 
-                href="https://dribbble.com/joey_harig"
-                rel="noopener noreferrer"
-            >
-                <Icon icon="dribble" />
-            </a>
+            <SocialLink
+                link="https://www.linkedin.com/in/joey-harig-256a05167/"
+                icon="linkedin"
+            />
+            <SocialLink
+                link="https://github.com/joeyHarig"
+                icon="github"
+            />
+            <SocialLink
+                link="https://dribbble.com/joey_harig"
+                icon="dribble"
+            />
         </div>
     );
 };
