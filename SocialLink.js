6b414a295c193a042c0fe313f254d13d99244740
joeyHarig <55154968+joeyHarig@users.noreply.github.com>
2020-02-09 11:18:45 -0700

    GA Event tracking

---
+++ SocialLink.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactGA from 'react-ga';
+import Icon from '../icons/Icon';
+
+const SocialLink = props => {
+    
+    const trackGAEvent = ReactGA.event({
+        category: 'Link Click',
+        action: `Visited ${props.icon} profile`
+    });
+
+    return (
+        <a 
+            target="_blank"
+            href={props.link}
+            rel="noopener noreferrer"
+            onClick={trackGAEvent}
+        >
+            <Icon icon={props.icon} />
+        </a>
+    );
+};
+
+export default SocialLink;
